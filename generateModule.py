6dd3e8a47c0197e71690895ce1356e23b9fd4691
ian <ianpmulhern@gmail.com>
2022-02-19 11:59:49 -0500

    updating generateModule.py to working copy that will render install-module.ps1

--- generateModule.py
+++ generateModule.py
@@ -1,27 +1,16 @@
-'''Copyright 2015 Christopher Young (@netmanchris)
-Licensed under the Apache License, Version 2.0 (the “License”); you may not use this file except in compliance with the 
-License. You may obtain a copy of the License at
-http://www.apache.org/licenses/LICENSE-2.0
-Unless required'''
+import jinja2
 
-from jinja2 import Environment, FileSystemLoader, Template
-import yaml
-import json
-
-ENV = Environment(loader=FileSystemLoader('./'))
-
-with open("dhcp_scopes.yml") as inputfile:
-    dhcpscopes =  yaml.load(inputfile)
+templateLoader = jinja2.FileSystemLoader(searchpath="./")
+templateEnv = jinja2.Environment(loader=templateLoader)
+TEMPLATE_FILE = "Install-Module.ps1"
+template = templateEnv.get_template(TEMPLATE_FILE)
 
 
-template = ENV.get_template("add_dhcp_ps.j2")
-#print (template.render(devglobals=devglobals, dev=dev))
-with open("./final_dhcp.ps1", "w") as file:
-    file.write(template.render(dhcpscopes=dhcpscopes))
+data = {
+    "module_name" = "MyModule"
+}
 
-# Print dictionary generated from yaml
+outputText = template.render(data)
 
-
-# Render template and print generated config to console
-#template = ENV.get_template("Comware5_Template.j2")
-#print (template.render(network_global=network_global, device_values = device_values, site= site))
+# maybe i can functionize the above and loop through module files to render?
+# need to research more jinja2 capabilities.

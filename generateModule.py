666d70b89260e8b78e7c699456d373ab0fa46061
ian <ianpmulhern@gmail.com>
2022-02-21 10:24:45 -0500

    Function for generateModule fixed

--- generateModule.py
+++ generateModule.py
@@ -4,7 +4,7 @@ data = {
     "module_name": "MyModule"
 }
 
-def renderTemplate(TEMPLATE_FILE)
+def renderTemplate(TEMPLATE_FILE):
     templateLoader = jinja2.FileSystemLoader(searchpath="./")
     templateEnv = jinja2.Environment(loader=templateLoader)
     template = templateEnv.get_template(TEMPLATE_FILE)
@@ -12,9 +12,9 @@ def renderTemplate(TEMPLATE_FILE)
 
 
 TEMPLATE_FILE = "Install-Module.ps1"
-renderTemplate(TEMPLATE_FILE)
+outputText = renderTemplate(TEMPLATE_FILE)
 
-outputText = template.render(data)
+print(outputText)
 
 # maybe i can functionize the above and loop through module files to render?
 # need to research more jinja2 capabilities.

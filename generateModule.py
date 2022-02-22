b62780aec64f0674743cb5250b1dd3ec1e6b225b
ian <ianpmulhern@gmail.com>
2022-02-22 08:49:03 -0500

    Updated generateModule.py to handle cli arguments.

--- generateModule.py
+++ generateModule.py
@@ -1,6 +1,7 @@
 import jinja2
 import os
 import pathlib
+import argparse
 
 data = {
     "module_name": "MyModule",
@@ -31,15 +32,25 @@ def renameModuleFiles(templateFilePath):
 
 
 # main
+if __name__ == '__main__':
 
-os.rename('./TemplateModule/module_name', f'./TemplateModule/{data["module_name"]}')
+    # cli arguments for module and friendly names
+    parser = argparse.ArgumentParser(description="Pass names for module name for folders and friendly name for cmdlets e.g. --mName WidgetCorp --fName WC")
+    parser.add_argument('--mName', '-m', required=True, help='Module name for directories and import-module.')
+    parser.add_argument('--fName', '-f', required=True, help='Friendly name for cmdlets e.g. WC gives Set-WCConfiguration.')
 
-templates = getTemplates('TemplateModule', '1')
+    args = parser.parse_args()
+    data['module_name'] = args.mName 
+    data['friendly_name'] = args.fName 
 
-for template in templates:
-    p = pathlib.PureWindowsPath(template)
-    outputText = renderTemplate(p.as_posix())
-    with open(template, 'w') as outputFile:
-        outputFile.write(outputText)
-    os.rename(template, renameModuleFiles(template))
-     # print(outputText)
+    os.rename('./TemplateModule/module_name', f'./TemplateModule/{data["module_name"]}')
+
+    templates = getTemplates('TemplateModule', '1')
+
+    for template in templates:
+        p = pathlib.PureWindowsPath(template)
+        outputText = renderTemplate(p.as_posix())
+        with open(template, 'w') as outputFile:
+            outputFile.write(outputText)
+        os.rename(template, renameModuleFiles(template))
+         # print(outputText)
